fccd39a58713653a62d94c16d5d8b23feb302d2f
Uday-Bairapaka <udaybairapaka666@gmail.com>
2024-01-10 12:49:53 +0530

    Your commit message here

--- src/Works/works.js
+++ src/Works/works.js
@@ -10,8 +10,13 @@ import Portfolio6 from '../assets/portfolio-6.png';
 const Works = () => {
     return (
         <section id="works"> 
-            <h2 className="workstitle">My Portfolio</h2>
-            <span className="worksDesc">I am a Skilled and passionate web designer with experience in creating visually appearing and user-frienldy website Ihave a strong understanding of design and a keeneye for detail. I am proficient in HTML, CSS, and Javascript usahjhj</span>
+            <h2 className="workstitle">Personal Details</h2>
+            <span className="worksDesc"><strong>Name :</strong> Uday Bairapaka <br />
+            <strong>Father’s Name :</strong> Ramesh Bairapaka<br />
+            <strong>Languages :</strong> English, Telugu and Hindi<br />
+            <strong>Mobile No :</strong> 6301726717<br />
+            <strong>Address :</strong> H.No:3-5-24/23/97/3 Bakshiguda, HB Colony, Moula Ali,
+Hyderabad-40</span>
             <div className="worksImgs">
                 <img src={Portfolio1} alt="" className="worksImg"/>
                 <img src={Portfolio2} alt="" className="worksImg"/>
@@ -20,7 +25,7 @@ const Works = () => {
                 <img src={Portfolio5} alt="" className="worksImg"/>
                 <img src={Portfolio6} alt="" className="worksImg"/>
             </div>
-            <button className="workbtn">See More</button>
+            <button className="workbtn"><a href="https://drive.google.com/file/d/1_4EtreZrHgQpXJ0hBvcITYr8hzlx3xLw/view?usp=sharing">CV Link</a></button>
         </section>
     )
 }
